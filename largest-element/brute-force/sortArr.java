18b84d60b29478adcdbf1cd5aac8752d7a639666
Tanisha Ali <tanishaali89@gmail.com>
2026-08-10 23:46:22 +0530

    Largest element of array

---
+++ largest-element/brute-force/sortArr.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class sortArr {
+    public static int sortArr(int[] arr) {
+        Arrays.sort(arr);
+        return arr[arr.length - 1];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int largestElement = sortArr(arr);
+        System.out.println("The largest element in the array is: " + largestElement);
+    }
+}
